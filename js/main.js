0a81012f69154e63bd15b4610ee428509af83f58
Andrea Fermo <andreafermo.sd@gmail.com>
2023-02-22 15:34:32 +0100

    Progetto consluso

--- js/main.js
+++ js/main.js
@@ -1,30 +1,35 @@
 const kmPrice = 0.21;
-console.log(kmPrice)
+console.log(kmPrice);
 
 let distance = parseInt(prompt('Quanti kilometri vuoi percorrere?'));
-console.log(distance)
+document.getElementById('distance').innerHTML = `${distance} km`;
+console.log(distance);
 
 let age = parseInt(prompt('Quanti anni hai?'));
-console.log(age)
+document.getElementById('age').innerHTML = `${age} anni`;
+console.log(age);
 
 const totalPrice = kmPrice * distance;
-console.log(totalPrice)
+document.getElementById('totalPrice').innerHTML = `${totalPrice} &#8364`;
+console.log(totalPrice);
 
 const underageDiscont = totalPrice * 0.8;
-const uDiscountRounded = underageDiscont.toFixed(2)
+const uDiscountRounded = underageDiscont.toFixed(2);
 
 
 const retiredDiscount = totalPrice * 0.6;
-const rDiscountRounded = retiredDiscount.toFixed(2)
+const rDiscountRounded = retiredDiscount.toFixed(2);
 
 if (isNaN(distance) || isNaN(age)) {
     console.log('I dati inseriti devo essere numeri! se visualizzi questo messaggio aggiorna la pagina e riprova!')
 } else {
     if (age < 18) {
+        document.getElementById('discountedPrice').innerHTML = `Biglietto ridotto - sotto i 18 anni: ${uDiscountRounded} &#8364`;
         console.log(uDiscountRounded);
     } else if (age >= 65) {
+        document.getElementById('discountedPrice').innerHTML = `Biglietto ridotto - 65 anni compiuti o più: ${retiredDiscount} &#8364`;
         console.log(rDiscountRounded)
     } else {
-        console.log(totalPrice)
+        document.getElementById('discountedPrice').innerHTML = `Nessuna scontistica applicabile`;
     }
 }
